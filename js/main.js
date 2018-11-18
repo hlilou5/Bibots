3c9eaef6b453ea0d65a834ead2d7f1f0dc6a53f6
hlilou5 <40966905+hlilou5@users.noreply.github.com>
2018-11-18 22:11:55 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function submit() {
+	var x = document.getElementById('user').value;
+	var y = document.getElementById('pass').value;
+	if (x === "hlilou",y === "hlilou") {
+		document.getElementById("login-div").style.display="none";
+		document.getElementById("logedin-div").style.display="block";
+		document.getElementById('name0').innerHTML=x;
+	}
+
+	if (x === "zeineb",y === "hlilou") {
+		document.getElementById("login-div").style.display="none";
+		document.getElementById("logedin-div").style.display="block";
+		document.getElementById('name0').innerHTML=x;
+	}
+
+	if (x === "nour",y === "hlilou") {
+		document.getElementById("login-div").style.display="none";
+		document.getElementById("logedin-div").style.display="block";
+		document.getElementById('name0').innerHTML=x;
+	}
+
+	if (x === "sarah",y === "hlilou") {
+		document.getElementById("login-div").style.display="none";
+		document.getElementById("logedin-div").style.display="block";
+		document.getElementById('name0').innerHTML=x;
+	}
+}
